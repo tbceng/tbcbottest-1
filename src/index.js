95a80055314e62de250c68574cbd4609e12c0153
aubrcart224 <aubrcart224@ed.amdsb.ca>
2023-10-10 21:15:23 -0400

    init init

--- src/index.js
+++ src/index.js
@@ -23,14 +23,30 @@ client.on('messageCreate', (message)=> {
     console.log(message.content);
 });
 
-// check if a message is hello and respond with hello back 
+// check if a message is pong and respond with ping back 
 client.on('messageCreate', (message) => {
     if (message.content == 'pong'){
         message.reply('ping')
     }
 });
 
+// check if a message if yes make respond
+client.on('messageCreate', (message) => {
+    if (message.content == 'how to code a discord bot'){
+        message.reply('Idk man try google or something')
+    }
+});
+
+
+//check if cleint says ball and respond with balls 
+client.on('messageCreate', (message) => {
+    if (message.content == 'ball'){
+        message.reply("balls")
+    }
+});
+
 //login and token for the bot 
+//token is hidden 
 client.login(process.env.TOKEN);
 
 
