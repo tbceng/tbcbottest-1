151d3e7e315f764250ceb940b73851e032f934e0
oxzoid <pranavdevkhindria@gmail.com>
2023-11-14 02:23:32 +0530

    commenting the mood command for now

--- model.js
+++ model.js
@@ -126,91 +126,91 @@ bot.on('messageCreate', async message => {
   }
 });
 
-bot.on('messageCreate', async message => {
-  async function isImageUrl(url) {
-    try {
-      // Fetch the image headers
-      const response = await fetch(url, { method: 'HEAD' });
+// bot.on('messageCreate', async message => {
+//   async function isImageUrl(url) {
+//     try {
+//       // Fetch the image headers
+//       const response = await fetch(url, { method: 'HEAD' });
 
-      // Check if the request was successful (status code 2xx)
-      if (!response.ok) {
-        return false;
-      }
+//       // Check if the request was successful (status code 2xx)
+//       if (!response.ok) {
+//         return false;
+//       }
 
-      // Get the content type from the response headers
-      const contentType = response.headers.get('Content-Type');
+//       // Get the content type from the response headers
+//       const contentType = response.headers.get('Content-Type');
 
-      // Check if the content type indicates an image and the URL ends with a valid image file extension
-      return contentType.startsWith('image/') && /\.(jpg|jpeg|png|gif|bmp|svg)$/i.test(url);
-    } catch (error) {
-      // Handle fetch errors (e.g., network issues)
-      console.error('Error checking image URL:', error);
-      return false;
-    }
-  }
-  if (message.content.startsWith('!mood ')) {
-    const commandPrefix = '!mood ';
-    const command = commandPrefix;
-    const question = message.content.slice(commandPrefix.length).trim();
-    console.log(command, question);
-    console.log("Command:", command);
-    console.log("\nQuestion:", question);
-    if (!isImageUrl(question)) return message.channel.send(` Please include a link to the image`);
-    const imageUrl = question;
-    const apiKey = process.env.GOOGLE_API;
-    async function detectFacesInImageUrlAndLogToConsole(apiKey, imageUrl) {
-        const image = new Image();
+//       // Check if the content type indicates an image and the URL ends with a valid image file extension
+//       return contentType.startsWith('image/') && /\.(jpg|jpeg|png|gif|bmp|svg)$/i.test(url);
+//     } catch (error) {
+//       // Handle fetch errors (e.g., network issues)
+//       console.error('Error checking image URL:', error);
+//       return false;
+//     }
+//   }
+//   if (message.content.startsWith('!mood ')) {
+//     const commandPrefix = '!mood ';
+//     const command = commandPrefix;
+//     const question = message.content.slice(commandPrefix.length).trim();
+//     console.log(command, question);
+//     console.log("Command:", command);
+//     console.log("\nQuestion:", question);
+//     if (!isImageUrl(question)) return message.channel.send(` Please include a link to the image`);
+//     const imageUrl = question;
+//     const apiKey = process.env.GOOGLE_API;
+//     async function detectFacesInImageUrlAndLogToConsole(apiKey, imageUrl) {
+//         const image = new Image();
       
-        await image.loadImage(imageUrl);
+//         await image.loadImage(imageUrl);
       
-        const request = {
-          image: {
-            content: await image.toDataURL(),
-          },
-          features: [
-            {
-              type: 'FACE_DETECTION',
-              maxResults: 10,
-            },
-          ],
-        };
+//         const request = {
+//           image: {
+//             content: await image.toDataURL(),
+//           },
+//           features: [
+//             {
+//               type: 'FACE_DETECTION',
+//               maxResults: 10,
+//             },
+//           ],
+//         };
       
-        const response = await client.annotateImage(request);
+//         const response = await client.annotateImage(request);
       
-        const faceAnnotations = response.responses[0].faceAnnotations;
+//         const faceAnnotations = response.responses[0].faceAnnotations;
       
-        for (const faceAnnotation of faceAnnotations) {
-          const boundingBox = faceAnnotation.boundingBox;
+//         for (const faceAnnotation of faceAnnotations) {
+//           const boundingBox = faceAnnotation.boundingBox;
       
-          // Draw a bounding box around the face
-          image.getContext('2d').strokeStyle = 'red';
-          image.getContext('2d').lineWidth = 5;
-          image.getContext('2d').strokeRect(
-            boundingBox.vertices[0].x,
-            boundingBox.vertices[0].y,
-            boundingBox.vertices[2].x - boundingBox.vertices[0].x,
-            boundingBox.vertices[2].y - boundingBox.vertices[0].y
-          );
+//           // Draw a bounding box around the face
+//           image.getContext('2d').strokeStyle = 'red';
+//           image.getContext('2d').lineWidth = 5;
+//           image.getContext('2d').strokeRect(
+//             boundingBox.vertices[0].x,
+//             boundingBox.vertices[0].y,
+//             boundingBox.vertices[2].x - boundingBox.vertices[0].x,
+//             boundingBox.vertices[2].y - boundingBox.vertices[0].y
+//           );
       
-          // Log the face's identity
-          console.log(faceAnnotation.landmarkAnnotations.find((landmarkAnnotation) => landmarkAnnotation.type === 'NOSE_TIP').position);
-        }
+//           // Log the face's identity
+//           console.log(faceAnnotation.landmarkAnnotations.find((landmarkAnnotation) => landmarkAnnotation.type === 'NOSE_TIP').position);
+//         }
       
-        // Display the image with the bounding boxes drawn on it
-        // This can be done by creating a canvas element and setting its image source to the image data from the Canvas object
-        // You can then append the canvas element to the DOM
-      }      
+//         // Display the image with the bounding boxes drawn on it
+//         // This can be done by creating a canvas element and setting its image source to the image data from the Canvas object
+//         // You can then append the canvas element to the DOM
+//       }      
       
-      // Example usage:
+//       // Example usage:
       
       
-      await detectFacesInImageUrlAndLogToConsole(apiKey, imageUrl);
-    // axios.post(`https://generativelanguage.googleapis.com/v1beta2/models/chat-bison-001:generateMessage?key=${API_KEY}`,{
-    //     params:{
-    //         "prompt": {"messages": [{"content":"hi"}]}
-    //     }
-    // })
-  }
-});
+//       await detectFacesInImageUrlAndLogToConsole(apiKey, imageUrl);
+//     // axios.post(`https://generativelanguage.googleapis.com/v1beta2/models/chat-bison-001:generateMessage?key=${API_KEY}`,{
+//     //     params:{
+//     //         "prompt": {"messages": [{"content":"hi"}]}
+//     //     }
+//     // })
+//   }
+// });
 
 bot.login(process.env.TOKEN);
